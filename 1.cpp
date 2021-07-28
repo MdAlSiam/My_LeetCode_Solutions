3493eb99a4fd886cdb52a005f0571f949326d78a
Md. Al Siam <highambitioussiam@gmail.com>
2021-07-29 01:37:46 +0600

    LeetCode: Two Sum
    Problem Link: https://leetcode.com/problems/two-sum/

--- 1.cpp
+++ 1.cpp
@@ -3,6 +3,7 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> ans;
         ans.clear();
+        
         bool found = false;
         for (int i = 0; i < nums.size(); i++) {
             for (int j = i+1; j < nums.size(); j++) {
