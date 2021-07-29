0ed16bc150277986fc240454e59f94254a990893
MdAlSiam <md.al.siam.008@gmail.com>
2021-07-29 10:00:52 +0600

    LeetCode 3Sum Closest

---
+++ 16.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int minDiff = 10000000;
+        int ans;
+        for (int i = 0; i < nums.size()-2; i++) {
+            int lo = i+1;
+            int hi = nums.size()-1;
+
+            while (lo < hi) {
+                int sum = nums[i]+nums[lo]+nums[hi];
+
+                if (abs(target-sum) < minDiff) {
+                    minDiff = abs(target-sum);
+                    ans = sum;
+                }
+
+                if (sum < target) lo++;
+                else hi--;
+            }
+        }
+
+        return ans;
+    }
+};
+int main() {
+    Solution s;
+    // Call s
+}
