822f56d50bee066db28fc6b32132a3afde554416
Md. Al Siam <highambitioussiam@gmail.com>
2022-04-14 05:46:03 +0600

    Time: 0 ms (100.00%), Space: 6.6 MB (18.18%) - LeetHub

---
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        int r1 = 0;
+        int r2 = n-1;
+        int c1 = 0;
+        int c2 = n-1;
+
+        vector<vector<int>> v(n, vector<int>(n));
+
+        int val = 0;
+
+        while (r1 <= r2 and c1 <= c2) {
+            // left to right
+            for (int i = c1; i <= c2; i++) {
+                v[r1][i] = ++val;
+            }
+
+            // up to down
+            for (int i = r1+1; i <= r2; i++) {
+                v[i][c2] = ++val;
+            }
+
+
+            // right to left
+            for (int i = c2-1; i > c1; i--) {
+                v[r2][i] = ++val;
+            }
+
+            // down to up
+            for (int i = r2; i > r1; i--) {
+                v[i][c1] = ++val;
+            }
+
+            r1++;
+            r2--;
+            c1++;
+            c2--;
+        }
+
+        return v;
+    }
+};
