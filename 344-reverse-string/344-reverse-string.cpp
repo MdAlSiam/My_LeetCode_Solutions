d3074be3158baf8fd5cdfd6c063264eec723e429
Md. Al Siam <highambitioussiam@gmail.com>
2022-04-14 06:03:48 +0600

    Time: 16 ms (96.85%), Space: 23.2 MB (41.86%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        
+        int lo = 0;
+        int hi = s.size()-1;
+
+        while (lo < hi) {
+            char temp = s[lo];
+            s[lo] = s[hi];
+            s[hi] = temp;
+            lo++;
+            hi--;
+        }
+    }
+};
+
