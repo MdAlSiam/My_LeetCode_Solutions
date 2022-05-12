4130963cc362fa93b991a46332f8b7bb0ee402ac
Md. Al Siam <highambitioussiam@gmail.com>
2022-05-12 22:33:07 +0600

    Time: 19 ms (39.65%), Space: 10.1 MB (33.16%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    vector<vector<int>> ans;
+
+    void printNums(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            printf(i == nums.size()-1 ? "%d\n" : "%d ", nums[i]);
+        }
+    }
+
+    void go(int position, vector<int>& nums) {
+        
+        if (position >= nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+
+        set<int> st;
+
+        for (int i = position; i < nums.size(); i++) {
+            if (st.find(nums[i]) != st.end()) continue;
+            st.insert(nums[i]);
+            
+            swap(nums[position], nums[i]); 
+            go(position+1, nums);
+            swap(nums[position], nums[i]);
+        }
+    }
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        go(0, nums);
+        return ans;
+    }
+};
