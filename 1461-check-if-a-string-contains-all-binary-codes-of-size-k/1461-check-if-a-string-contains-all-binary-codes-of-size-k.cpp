ad441164d54e46381db48dbb21154cfdf91a20f8
Md. Al Siam <highambitioussiam@gmail.com>
2022-05-31 13:03:07 +0600

    Time: 102 ms (88.36%), Space: 18.5 MB (94.34%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -3,17 +3,33 @@ public:
     bool hasAllCodes(string s, int k) {
         if (k > s.length()) return false;
         
-        set<string> st;
+        bool present[1<<k];
+        memset(present, false, sizeof(present));
         
-        for (int i = 0; i+k <= s.length(); i++) {
-            string curr = "";
-            for (int j = 0; j < k; j++) {
-                curr += s[i+j];
-            }
-            
-            st.insert(curr);          
+        int last_k_bits_mask = (1<<k)-1;
+        
+        int curr = 0;
+        for (int i = 0; i < k-1; i++) {
+            curr <<= 1;
+            if (s[i] == '1') curr |= 1;
         }
-
-        return st.size() == pow(2,k);
+        
+        for (int i = k-1; i < s.length(); i++) {
+            curr <<= 1;
+            if (s[i] == '1') curr |= 1;
+            curr &= last_k_bits_mask;
+            present[curr] = true;
+        }
+        
+        for (int i = 0; i < (1<<k); i++) {
+            if (not present[i]) {
+                return false;
+            }
+        }
+        
+        return true;
     }
 };
+
+
+// Idea Credit: https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/discuss/2093054/O(n)-Bitwise-100-fast-oror-Easy-to-understand-C%2B%2B
