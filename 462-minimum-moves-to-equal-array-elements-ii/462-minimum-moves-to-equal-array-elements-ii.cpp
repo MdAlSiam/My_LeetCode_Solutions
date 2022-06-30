dee33ccbb6f797cc9011d67d360a3ed188e05e43
Md. Al Siam <highambitioussiam@gmail.com>
2022-07-01 00:41:49 +0600

    Time: 15 ms (63.23%), Space: 10.9 MB (59.74%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -4,29 +4,27 @@ public:
 
         sort(nums.begin(), nums.end());
 
-        long long int fcs[nums.size()];
-        long long int total = 0;
+        int fcs[nums.size()]; // Cumulative Summation
+        int total = 0; // Total summation of the array nums
 
-        long long int prev = 0;
-
+        int prev = 0;
         for (int i = 0; i < nums.size(); i++) {
             fcs[i] = nums[i]+prev;
             prev = fcs[i];
             total += nums[i];
         }
 
-        long long int ans = LONG_LONG_MAX;
+        long long int ans = INT_MAX;
 
-        for (long long int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size(); i++) {
+            long long int n = nums.size();
             long long int num = nums[i];
-            long long int prev_sum = 0;
+            long int prev_sum = 0;
             if (i-1 >= 0) prev_sum = fcs[i-1];
             long long int next_sum = total-fcs[i];
-            long long int n = nums.size();
             long long int ansHere = next_sum-num*(n-i-1);
             long long int ii = i;
-            long long int temp = (long long int)(ii*num);
-            ansHere += temp-prev_sum;
+            ansHere += ii*num-prev_sum;
 
             ans = min(ans, ansHere);
         }
