fcd8d551e5f3818c57ee4baf1c5476962659174e
Md. Al Siam <highambitioussiam@gmail.com>
2022-07-02 10:17:40 +0600

    Time: 198 ms (59.32%), Space: 44.7 MB (96.61%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cs
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cs
@@ -11,23 +11,17 @@ public class Solution {
         long maxHorizSeg = 0;
         long maxVertSeg = 0;
 
-        // Console.Write("Horizontals "+horizontalCuts[0]+" ");
+
         for (int i = 1; i < horizontalCuts.Length; i++) {
             maxHorizSeg = Math.Max(maxHorizSeg, horizontalCuts[i]-horizontalCuts[i-1]);
-            // Console.Write(horizontalCuts[i]+" ");
         }
-        // Console.WriteLine();
 
-        // Console.Write("Verticals "+verticalCuts[0]+" ");
         for (int i = 1; i < verticalCuts.Length; i++) {
             maxVertSeg = Math.Max(maxVertSeg, verticalCuts[i]-verticalCuts[i-1]);
-            // Console.Write(verticalCuts[i]+" ");
         }
-        // Console.WriteLine();
 
         long mod = 1_000_000_007;
         
-        // long ans = ((maxHorizSeg%mod)*(maxVertSeg%mod))%mod;
         long ans = (maxHorizSeg*maxVertSeg)%mod;
 
         return (int) ans;
