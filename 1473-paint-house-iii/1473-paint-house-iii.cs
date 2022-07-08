469b40f5bef7af50747451aa164c26cbb682501a
Md. Al Siam <highambitioussiam@gmail.com>
2022-07-09 01:28:04 +0600

    Time: 208 ms (83.33%), Space: 42 MB (66.67%) - LeetHub

---
+++ 1473-paint-house-iii/1473-paint-house-iii.cs
@@ -0,0 +1,107 @@
+public class Solution {
+    bool debug = false;
+    
+    int[] houses = new int[] {};
+    int[][] cost = new int[][] {};
+    int m; // nHomes
+    int n; // nColors
+    int target;
+
+    int ans = 1_000_000_000;
+
+    int[,,] dp = new int[,,] {};
+
+    int go(int homeIndex, int colorIndex, int segmentSoFar) {
+        if (segmentSoFar > target) {
+            return 1_000_000_000;
+        }
+
+        if (homeIndex >= m) {
+            if (debug) Console.WriteLine(">"+segmentSoFar+" "+"X"+" target = "+target);
+
+            if (debug) {
+                for (int i = 0; i < m; i++) {
+                    Console.Write(houses[i]+"|");
+                } Console.WriteLine(" C1");
+            }
+
+            if (segmentSoFar == target) {
+                return 0;
+            }
+
+            return 1_000_000_000;
+        }
+
+        if (dp[homeIndex, colorIndex, segmentSoFar] != -1) {
+            if (debug) 
+                Console.WriteLine("DP->"+homeIndex+" "+colorIndex+" :"+dp[homeIndex,colorIndex,segmentSoFar]);
+            if (debug) {
+                for (int i = 0; i < m; i++) {
+                    Console.Write(houses[i]+" ");
+                } Console.WriteLine(" C2");
+            }
+
+            return dp[homeIndex, colorIndex, segmentSoFar];
+        }
+
+        int ret = 1_000_000_000;
+        // int ret2 = 1_000_000_000;
+        // int ret3 = 1_000_000_000;
+        // int ret4 = 1_000_000_000;
+
+        if (houses[homeIndex] == 0) {
+            for (int i = 0; i < n; i++) {
+                if (i+1 != colorIndex) {
+                    houses[homeIndex] = i+1;
+                    ret = Math.Min(ret, cost[homeIndex][i] + go(homeIndex+1, i+1, segmentSoFar+1));
+                    houses[homeIndex] = 0;
+                }
+                else {
+                    houses[homeIndex] = i+1;
+                    ret = Math.Min(ret, cost[homeIndex][i] + go(homeIndex+1, i+1, segmentSoFar));
+                    houses[homeIndex] = 0;
+                }
+            }
+        }
+        else {
+            if (houses[homeIndex] != colorIndex) {
+                ret = Math.Min(ret, go(homeIndex+1, houses[homeIndex], segmentSoFar+1));
+            }
+            else {
+                ret = Math.Min(ret, go(homeIndex+1, houses[homeIndex], segmentSoFar));
+            }
+        }
+
+        // int retval = 1_000_000_000;
+        // retval = Math.Min(retval, ret1);
+        // retval = Math.Min(retval, ret2);
+        // retval = Math.Min(retval, ret3);
+        // retval = Math.Min(retval, ret4);
+
+        return dp[homeIndex, colorIndex, segmentSoFar] = ret;
+    }
+
+    public int MinCost(int[] _houses, int[][] _cost, int _m, int _n, int _target) {
+        houses = _houses;
+        cost = _cost;
+        m = _m;
+        n = _n;
+        target = _target;
+
+        dp = new int[m+10,n+10,target+4];
+
+        for (int i = 0; i < m+10; i++) 
+            for (int j = 0; j < n+10; j++) 
+                for (int k = 0; k < target+4; k++)
+                    dp[i,j,k] = -1;
+
+        if (debug) Console.WriteLine("DP Initiated: "+(m+10-1)+" "+(n+10-1));
+
+        // int homeIndex, int colorIndex, int segmentSoFar
+        ans = go(0, 0, 0);
+        
+        if (ans == 1_000_000_000) return -1;
+
+        return ans;
+    }
+}
