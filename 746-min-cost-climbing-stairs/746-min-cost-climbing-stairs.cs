198307c0a5ce345c1bed8e62f84e51e4e6a29a9b
Md. Al Siam <highambitioussiam@gmail.com>
2022-07-10 21:26:12 +0600

    Time: 176 ms (8.00%), Space: 38 MB (42.00%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cs
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cs
@@ -1,3 +1,4 @@
+/*
 public class Solution {
     public int MinCostClimbingStairs(int[] cost) {
         int[] dp = new int[cost.Length+1];
@@ -10,3 +11,22 @@ public class Solution {
         return dp[cost.Length];
     }
 }
+*/
+
+public class Solution {
+    public int MinCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.Length+1];
+        Array.Fill(dp, 0);
+        
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for (int i = 2; i < cost.Length+1; i++) {
+            int add = Math.Min(dp[i-1], dp[i-2]);
+            dp[i] = (i < cost.Length ? cost[i] : 0) + add;
+        }
+
+        return dp[cost.Length];
+    }
+}
+
