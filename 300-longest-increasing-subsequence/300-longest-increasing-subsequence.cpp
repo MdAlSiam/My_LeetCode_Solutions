f1b4cf52f821debaf003cce3527e96ab2bcd1e10
Md. Al Siam <highambitioussiam@gmail.com>
2022-08-09 05:13:41 +0600

    Time: 338 ms (61.15%), Space: 10.3 MB (97.96%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -7,15 +7,17 @@ public:
             dp[i] = 1;
         }
 
-        int ans = 1;
-
         for (int i = 0; i < nums.size(); i++) {
             for (int j = i+1; j < nums.size(); j++) {
                 if (nums[j] > nums[i]) {
                     dp[j] = max(dp[j], dp[i]+1);
-                    ans = max(ans, dp[j]);
                 }
             }
+        }
+
+        int ans = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            ans = max(ans, dp[i]);
         }
 
         return ans;
