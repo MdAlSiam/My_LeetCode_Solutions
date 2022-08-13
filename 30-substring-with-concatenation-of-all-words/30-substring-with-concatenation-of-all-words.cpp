1cfd5745783a44251c0d1a856bc06da29bb391b4
Md. Al Siam <highambitioussiam@gmail.com>
2022-08-14 01:43:08 +0600

    Time: 361 ms (58.15%), Space: 22.5 MB (78.24%) - LeetHub

---
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int> ans;
+
+        int winSize = words.size()*words[0].size();
+        int wordSize = words[0].size();
+
+        map<string, int> mp;
+
+        for (string word : words) {
+            mp[word]++;
+        }
+
+        for (int i = 0; i+winSize <= s.length(); i++) { 
+            map<string, int> new_mp;
+            new_mp.clear();
+
+            string temp = "";
+            int kount = wordSize;
+            for (int j = 0; j < winSize; j++) {
+                temp += s[i+j];
+                kount--;
+
+                if (kount == 0) {
+                    if (mp.count(temp)) {
+                        new_mp[temp]++;
+                        
+                        temp = "";
+                        kount = wordSize;
+                    }
+                    else {
+                        break;
+                    }
+                }
+                
+            }
+
+            bool ok = true;
+            for (auto element : mp) {
+                if (new_mp.count(element.first) and new_mp[element.first] == element.second);
+                else {ok = false; break;}
+            }
+
+            if (ok) ans.push_back(i);
+        }
+
+        return ans;
+    }
+};
