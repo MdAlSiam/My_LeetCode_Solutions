5515510e89cb42457261fe2c887d3f695c7021ea
Md. Al Siam <highambitioussiam@gmail.com>
2022-11-12 17:55:10 +0600

    Time: 26 ms (32.41%), Space: 21.5 MB (38.59%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.cpp
@@ -0,0 +1,41 @@
+class Solution {
+    typedef struct{
+        TreeNode* node;
+        int sumHere;
+    } myNode;
+
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (root == nullptr) return false;
+
+        myNode aNode;
+        aNode.node = root;
+        aNode.sumHere = root->val;
+
+        queue<myNode> q;
+        q.push(aNode);
+
+        while (!q.empty()) {
+            myNode curr = q.front();
+            q.pop();
+
+            if (curr.sumHere == targetSum and curr.node->left == nullptr and curr.node->right == nullptr) {
+                return true;
+            }
+
+            if (curr.node->left) {
+                aNode.node = curr.node->left;
+                aNode.sumHere = curr.sumHere + curr.node->left->val;
+                q.push(aNode);
+            }
+
+            if (curr.node->right) {
+                aNode.node = curr.node->right;
+                aNode.sumHere = curr.sumHere + curr.node->right->val;
+                q.push(aNode);
+            }
+        }
+
+        return false;
+    }
+};
