7cb85e1b95da49aee46a8d4ac2c659bb454f4c3d
Md. Al Siam <highambitioussiam@gmail.com>
2022-11-22 22:45:45 +0600

    Time: 129 ms (98.79%), Space: 8.7 MB (99.21%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        bool isComposite[max(2, n+1)];
-        for (int i = 1; i <= n; i++) isComposite[i] = false;
+        bool isComposite[n/2 + 3];
+        for (int i = 0; i < n/2 + 3; i++) isComposite[i] = false;     
 
-        isComposite[0] = isComposite[1] = true;
-
-        for (int i = 2; i*i <= n; i++) {
-            if (isComposite[i] == false) {
-                for (int times = 2; i*times <= n; times++) {
-                    isComposite[i*times] = true;
+        for (int i = 3; i*i <= n; i += 2) {
+            if (isComposite[i/2] == false) {
+                for (int times = i; i*times <= n; times += 2) {
+                    isComposite[(i*times)/2] = true;
                 }
             }
         }
 
         int ans = 0;
-        for (int i = 1; i < n; i++) if (isComposite[i] == false) ans++;
+        if (n > 2) ans = 1;
+        for (int i = 3; i < n; i += 2) if (isComposite[i/2] == false) ans++;
         return ans;
     }
 };
