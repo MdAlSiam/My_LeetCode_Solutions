be021ac9b26108c87d4e39998b5f900c71796ab8
Md. Al Siam <highambitioussiam@gmail.com>
2023-02-05 21:46:13 +0600

    Time: 0 ms (100.00%), Space: 8.3 MB (30.12%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int lo = 0;
+        int hi = nums.size()-1;
+
+        int mid = 0;
+        while (mid <= hi) {
+            if (nums[mid] == 0) {
+                swap(nums[mid], nums[lo]);
+                lo++;
+                mid++;
+            }
+            else if (nums[mid] == 1) {
+                mid++;
+            } 
+            else if (nums[mid] == 2) {
+                swap(nums[mid], nums[hi]);
+                hi--;
+            }
+        }
+    }
+};
